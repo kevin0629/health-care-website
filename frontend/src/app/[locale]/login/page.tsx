f99180475a1a90fcbdc9194666d0776aaf666b3e
Kevin Kuo <k113423027@g.ncu.edu.tw>
2025-01-01 17:50:22 +0800

    change oauth client id

--- frontend/src/app/[locale]/login/page.tsx
+++ frontend/src/app/[locale]/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   const url = new URL("https://portal.ncu.edu.tw/oauth2/authorization");
   url.searchParams.append("response_type", "code");
   url.searchParams.append("state", "0");
-  url.searchParams.append("client_id", "20240524005413cH6En5RFZc35");
+  url.searchParams.append("client_id", "20250101165455gNN4VJXGPlJb");
   url.searchParams.append("redirect_uri", `${BACKEND_HOST}/api/auth/return-to`);
   url.searchParams.append("scope", "identifier chinese-name");
 
