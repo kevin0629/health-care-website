df544908bb5540f4a3c0e4e34e7507934379ea24
Kevin Kuo <k113423027@g.ncu.edu.tw>
2025-07-17 23:33:55 +0800

    add header

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -17,5 +17,18 @@ export default withNextIntl({
                 hostname: "**",
             }
         ]
+    },
+
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'Referrer-Policy',  value: 'strict-origin-when-cross-origin'},
+                    { key: 'X-Content-Type-Options', value: 'nosniff' },
+                    { key: 'X-Frame-Options', value: 'DENY' }
+                ]
+            }
+        ];
     }
 });
